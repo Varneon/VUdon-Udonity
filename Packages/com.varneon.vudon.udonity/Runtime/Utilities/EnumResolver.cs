7354aec14f098749c1467e78fe65e750598aa8cd
Varneon <me@varneon.com>
2022-12-21 20:54:33 -0800

    Add enum resolver

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Utilities/EnumResolver.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.EnumResolver
+{
+    public static class UnityEngine_RenderingPath
+    {
+        public static object[] Items() => new object[]
+        {
+            RenderingPath.UsePlayerSettings,
+            RenderingPath.Forward,
+            RenderingPath.DeferredShading,
+            RenderingPath.VertexLit,
+            RenderingPath.DeferredLighting
+        };
+
+        public static int[] Values() => new int[] { 0, 3, 1, 2, 4 };
+
+        public static RenderingPath FromInt(int value) => (RenderingPath)Items()[value];
+
+        public static int ToInt(RenderingPath value) => Values()[(int)value + 1];
+    }
+
+    public static class UnityEngine_StereoTargetEyeMask
+    {
+        public static object[] Items() => new object[]
+        {
+            StereoTargetEyeMask.Both,
+            StereoTargetEyeMask.Left,
+            StereoTargetEyeMask.Right,
+            StereoTargetEyeMask.None
+        };
+
+        public static int[] Values() => new int[] { 3, 1, 2, 0 };
+
+        public static StereoTargetEyeMask FromInt(int value) => (StereoTargetEyeMask)Items()[value];
+
+        public static int ToInt(StereoTargetEyeMask value) => Values()[(int)value];
+    }
+
+    public static class UnityEngine_CameraClearFlags
+    {
+        public static object[] Items() => new object[]
+        {
+            CameraClearFlags.Skybox,
+            CameraClearFlags.SolidColor,
+            CameraClearFlags.Depth,
+            CameraClearFlags.Nothing
+        };
+
+        public static CameraClearFlags FromInt(int value) => (CameraClearFlags)Items()[value];
+
+        public static int ToInt(CameraClearFlags value) => (int)value - 1;
+    }
+}
