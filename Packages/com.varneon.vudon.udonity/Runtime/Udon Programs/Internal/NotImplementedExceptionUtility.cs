5f90ec8ba95c378702eea34e5d59a4a9f6eef808
Varneon <me@varneon.com>
2023-03-02 07:54:06 -0800

    Add NotImplementedExceptionUtility

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Internal/NotImplementedExceptionUtility.cs
@@ -0,0 +1,27 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Utilities
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class NotImplementedExceptionUtility : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private Logger.Abstract.UdonLogger logger;
+
+        public void ThrowStaticFlagsNotImplementedException()
+        {
+            logger.LogError("Static Editor Flags are not implemented!");
+        }
+
+        public void ThrowRotationToolNotImplementedException()
+        {
+            logger.LogError("Rotation tool is not implemented yet, only visual preview is available");
+        }
+
+        public void ThrowScaleToolNotImplementedException()
+        {
+            logger.LogError("Scale tool is not implemented yet, only visual preview is available");
+        }
+    }
+}
