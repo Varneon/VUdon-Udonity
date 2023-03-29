55c8c659fce04f78123af85cfec707f593f96b20
Varneon <me@varneon.com>
2023-03-29 07:46:11 -0700

    Add help box to root inclusion descriptor

--- Packages/com.varneon.vudon.udonity/Runtime/Scripts/UdonityRootInclusionDescriptor.cs
+++ Packages/com.varneon.vudon.udonity/Runtime/Scripts/UdonityRootInclusionDescriptor.cs
@@ -3,4 +3,15 @@
 namespace Varneon.VUdon.Udonity
 {
     public class UdonityRootInclusionDescriptor : MonoBehaviour { }
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+    [UnityEditor.CustomEditor(typeof(UdonityRootInclusionDescriptor))]
+    public class UdonityRootInclusionDescriptorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            UnityEditor.EditorGUILayout.HelpBox("This object hierarchy root will be included in Udonity for inspection on build.", UnityEditor.MessageType.Info);
+        }
+    }
+#endif
 }
