7436e029c2c58820603562ea12273914cdd09a7e
Varneon <me@varneon.com>
2023-05-24 07:30:22 -0700

    Add DropdownMenu

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/UI Elements/DropdownMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Varneon.VUdon.Udonity.UIElements
+{
+    public class DropdownMenu : Abstract.DropdownMenu
+    {
+        [SerializeField]
+        private Button[] eventProxies;
+
+        protected override void OnMenuItemInvoked(int index)
+        {
+            eventProxies[index].OnSubmit(null);
+        }
+    }
+}
