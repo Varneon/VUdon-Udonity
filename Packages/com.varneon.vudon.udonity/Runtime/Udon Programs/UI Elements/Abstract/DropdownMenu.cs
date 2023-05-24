9b2cbddea3e4d925f60c624d22bda483d1eee4b5
Varneon <me@varneon.com>
2023-05-24 07:28:56 -0700

    Add abstract DropdownMenu

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/UI Elements/Abstract/DropdownMenu.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Varneon.VUdon.Udonity.UIElements.Abstract
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class DropdownMenu : UdonSharpBehaviour
+    {
+        [SerializeField, HideInInspector]
+        internal Dropdown dropdown;
+
+        public void OnMenuItemInvoked()
+        {
+            OnMenuItemInvoked(dropdown.value);
+        }
+
+        protected abstract void OnMenuItemInvoked(int index);
+    }
+}
