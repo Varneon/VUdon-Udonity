e6f66c49583ede46e956f8f68d542a0bb91188ef
Varneon <me@varneon.com>
2023-05-24 06:05:11 -0700

    Add UndoTypes enum

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Enums/UndoTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace Varneon.VUdon.UdonUndo.Enums
+{
+    public enum GameObjectUndoType
+    {
+        Active,
+        Name,
+        Layer
+    }
+
+    public enum TransformUndoType
+    {
+        LocalPosition,
+        LocalRotation,
+        LocalScale
+    }
+}
