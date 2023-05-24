68277642a8ec1c584fbe2b319ced506cd70d39c6
Varneon <me@varneon.com>
2023-05-24 06:25:48 -0700

    Add UdonityPlayerScaleCallbackReceiver

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Internal/UdonityPlayerScaleCallbackReceiver.cs
@@ -0,0 +1,40 @@
+﻿using UdonSharp;
+using UnityEngine;
+using Varneon.VUdon.PlayerScaleUtility.Abstract;
+
+namespace Varneon.VUdon.Udonity.Utilities
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class UdonityPlayerScaleCallbackReceiver : PlayerScaleCallbackReceiver
+    {
+        [SerializeField]
+        private Pointer pointer;
+
+        [SerializeField]
+        private Logger.Abstract.UdonLogger logger;
+
+        [SerializeField]
+        private MeshRenderer handlePlanesRenderer;
+
+        [SerializeField, HideInInspector]
+        private Material handlePlanesMaterial;
+
+        private MaterialPropertyBlock handlePlanesMaterialPropertyBlock;
+
+        private void Start()
+        {
+            handlePlanesMaterialPropertyBlock = new MaterialPropertyBlock();
+        }
+
+        public override void OnPlayerScaleChanged(float newPlayerScale)
+        {
+            handlePlanesMaterialPropertyBlock.SetFloat("_CameraScale", newPlayerScale);
+
+            handlePlanesRenderer.SetPropertyBlock(handlePlanesMaterialPropertyBlock);
+
+            pointer.SetPlayerScale(newPlayerScale);
+
+            logger.Log($"[<color=#09C>PlayerScaleUtility</color>]: Avatar changed! New camera scale: <color=#6CC>{newPlayerScale}</color>");
+        }
+    }
+}
