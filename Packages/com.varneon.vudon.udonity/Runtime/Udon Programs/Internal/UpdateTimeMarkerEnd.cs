2ca98c10170767a9c66685775bd16e9566bd7a4c
Varneon <me@varneon.com>
2023-05-24 06:27:16 -0700

    Add profiler time markers

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Internal/UpdateTimeMarkerEnd.cs
@@ -0,0 +1,32 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Utilities
+{
+    [DefaultExecutionOrder(2147483647)]
+    public class UpdateTimeMarkerEnd : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private UpdateTimeMarkerStart startMarker;
+
+        private void FixedUpdate()
+        {
+            startMarker.OnFixedUpdateEnd();
+        }
+
+        private void Update()
+        {
+            startMarker.OnUpdateEnd();
+        }
+
+        private void LateUpdate()
+        {
+            startMarker.OnLateUpdateEnd();
+        }
+
+        public override void PostLateUpdate()
+        {
+            startMarker.OnPostLateUpdateEnd();
+        }
+    }
+}
---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Internal/UpdateTimeMarkerStart.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Utilities
+{
+    [DefaultExecutionOrder(-2146483648)]
+    public class UpdateTimeMarkerStart : UdonSharpBehaviour
+    {
+        public long[] Data => data;
+
+        private Stopwatch fixedUpdateStopwatch = new Stopwatch();
+
+        private Stopwatch updateStopwatch = new Stopwatch();
+
+        private Stopwatch lateUpdateStopwatch = new Stopwatch();
+
+        private Stopwatch postLateUpdateStopwatch = new Stopwatch();
+
+        private long[] data = new long[4];
+
+        private void FixedUpdate()
+        {
+            fixedUpdateStopwatch.Restart();
+        }
+
+        private void Update()
+        {
+            fixedUpdateStopwatch.Reset();
+
+            updateStopwatch.Restart();
+        }
+
+        private void LateUpdate()
+        {
+            lateUpdateStopwatch.Restart();
+        }
+
+        public override void PostLateUpdate()
+        {
+            postLateUpdateStopwatch.Restart();
+        }
+
+        internal void OnFixedUpdateEnd()
+        {
+            fixedUpdateStopwatch.Stop();
+
+            data[0] += fixedUpdateStopwatch.ElapsedTicks;
+        }
+
+        internal void OnUpdateEnd()
+        {
+            updateStopwatch.Stop();
+
+            data[1] = updateStopwatch.ElapsedTicks;
+        }
+
+        internal void OnLateUpdateEnd()
+        {
+            lateUpdateStopwatch.Stop();
+
+            data[2] = lateUpdateStopwatch.ElapsedTicks;
+        }
+
+        internal void OnPostLateUpdateEnd()
+        {
+            postLateUpdateStopwatch.Stop();
+
+            data[3] = postLateUpdateStopwatch.ElapsedTicks;
+
+            data[0] = 0;
+        }
+
+        internal void Activate()
+        {
+            //fixedUpdateStopwatch.Restart();
+            //updateStopwatch.Restart();
+            //lateUpdateStopwatch.Restart();
+            //postLateUpdateStopwatch.Restart();
+        }
+
+        internal void Deactivate()
+        {
+            fixedUpdateStopwatch.Reset();
+            updateStopwatch.Reset();
+            lateUpdateStopwatch.Reset();
+            postLateUpdateStopwatch.Reset();
+        }
+    }
+}
