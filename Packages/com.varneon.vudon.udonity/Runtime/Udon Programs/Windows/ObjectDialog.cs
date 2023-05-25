3f4cf40fb520163cfc24e65933f582eba3c8a401
Varneon <me@varneon.com>
2023-05-25 00:18:58 -0700

    Add object dialog class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/ObjectDialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using Varneon.VUdon.Udonity.Fields;
+
+namespace Varneon.VUdon.Udonity.Windows
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ObjectDialog : Abstract.ApplicationWindow
+    {
+        [SerializeField]
+        private GameObject listElement;
+
+        [SerializeField]
+        private RectTransform listRoot;
+
+        [SerializeField]
+        private TextMeshProUGUI headerTitle;
+
+        private ObjectField activeObjectField;
+
+        public void OpenWithType(ObjectField field, Type type)
+        {
+            headerTitle.text = string.Format("Select {0}", type.Name);
+
+            activeObjectField = field;
+
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            if (activeObjectField) { activeObjectField.EndObjectDialogSelection(); }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
