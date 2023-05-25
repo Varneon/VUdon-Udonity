3ac67815a976fa5977a4cd13de21c97d04cab80f
Varneon <me@varneon.com>
2023-05-25 01:15:12 -0700

    Add Udon monitor entry point element class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/Udon Monitor/UdonMonitorEntryPointElement.cs
@@ -0,0 +1,15 @@
+﻿using UdonSharp;
+
+namespace Varneon.VUdon.Udonity.Windows.UdonMonitor
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class UdonMonitorEntryPointElement : UdonSharpBehaviour
+    {
+        internal string methodName;
+
+        public void InvokeMethod()
+        {
+            GetComponentInParent<UdonMonitorWindow>().InvokeMethod(methodName);
+        }
+    }
+}
