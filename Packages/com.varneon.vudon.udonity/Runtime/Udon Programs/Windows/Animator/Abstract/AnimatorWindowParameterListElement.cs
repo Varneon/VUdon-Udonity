08ae002918bd642a3c04c4d57f19cf2fc4d54bcc
Varneon <me@varneon.com>
2023-05-25 00:50:12 -0700

    Add abstract animator parameter list element class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/Animator/Abstract/AnimatorWindowParameterListElement.cs
@@ -0,0 +1,14 @@
+﻿using UdonSharp;
+
+namespace Varneon.VUdon.Udonity.Windows.Animator.Abstract
+{
+    using Animator = UnityEngine.Animator;
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class AnimatorWindowParameterListElement : UdonSharpBehaviour
+    {
+        internal Animator targetAnimator;
+
+        protected int parameterHash;
+    }
+}
