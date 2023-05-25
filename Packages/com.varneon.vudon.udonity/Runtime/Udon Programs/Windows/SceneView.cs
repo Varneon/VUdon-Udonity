740a8b38a2be9b4aed3b605e980c6a3aee030875
Varneon <me@varneon.com>
2023-05-25 00:06:36 -0700

    Add scene view editor window class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/SceneView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Varneon.VUdon.Udonity.Windows.Scene;
+
+namespace Varneon.VUdon.Udonity.Windows
+{
+    public class SceneView : Abstract.EditorWindow
+    {
+        public override Vector2 MinSize => new Vector2(600f, 400f);
+
+        [SerializeField]
+        private SceneViewCamera sceneViewCamera;
+
+        protected override void OnWindowActiveStateChanged(bool active)
+        {
+            sceneViewCamera.gameObject.SetActive(active);
+        }
+    }
+}
