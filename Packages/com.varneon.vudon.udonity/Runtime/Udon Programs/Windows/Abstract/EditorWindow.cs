2d95305fb092f22fa3595a891d9535a276f88ea0
Varneon <me@varneon.com>
2023-05-25 00:02:27 -0700

    Add abstract editor window class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/Abstract/EditorWindow.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Windows.Abstract
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class EditorWindow : UdonSharpBehaviour
+    {
+        public abstract Vector2 MinSize { get; }
+
+        public void SetWindowActive(bool active)
+        {
+            OnWindowActiveStateChanged(active);
+
+            gameObject.SetActive(active);
+        }
+
+        protected virtual void OnWindowActiveStateChanged(bool active) { }
+    }
+}
