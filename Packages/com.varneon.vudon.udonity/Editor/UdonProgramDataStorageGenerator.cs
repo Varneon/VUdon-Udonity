f79dd18892cee05fbba8d7de5db003f269818d5a
Varneon <me@varneon.com>
2023-05-25 01:09:29 -0700

    Add Udon program data storage generator

---
+++ Packages/com.varneon.vudon.udonity/Editor/UdonProgramDataStorageGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+namespace Varneon.VUdon.UdonProgramDataStorage
+{
+    public class UdonProgramDataStorageGenerator
+    {
+        private const string
+            TYPE_ID_SYMBOL = "__refl_typeid",
+            TYPE_NAME_SYMBOL = "__refl_typename";
+
+        public static void GenerateProgramDataStorage()
+        {
+            IEnumerable<UdonBehaviour> behaviours = Resources.FindObjectsOfTypeAll<UdonBehaviour>().Where(u => u.gameObject.scene.IsValid());
+
+            HashSet<AbstractUdonProgramSource> programSources = new HashSet<AbstractUdonProgramSource>(behaviours.Select(b => b.programSource));
+
+            List<string> programInfos = new List<string>();
+
+            List<string[]> programSymbols = new List<string[]>();
+
+            List<Type[]> programSymbolTypes = new List<Type[]>();
+
+            List<string[]> programEntryPoints = new List<string[]>();
+
+            foreach (AbstractUdonProgramSource programSource in programSources)
+            {
+                if (programSource == null) { continue; }
+
+                IUdonProgram program = programSource.SerializedProgramAsset.RetrieveProgram();
+
+                IUdonSymbolTable symbolTable = program.SymbolTable;
+
+                System.Collections.Immutable.ImmutableArray<string> symbols = symbolTable.GetSymbols();
+
+                IUdonHeap heap = program.Heap;
+
+                string header = programSource.name;
+
+                if(symbols.Contains(TYPE_ID_SYMBOL) && symbols.Contains(TYPE_NAME_SYMBOL))
+                {
+                    long id = heap.GetHeapVariable<long>(symbolTable.GetAddressFromSymbol(TYPE_ID_SYMBOL));
+                    string name = heap.GetHeapVariable<string>(symbolTable.GetAddressFromSymbol(TYPE_NAME_SYMBOL));
+
+                    header = $"{name} {id}";
+                }
+                else
+                {
+                    // Program is not UdonSharpBehaviour
+
+                    continue;
+                }
+
+                programInfos.Add(header);
+
+                IEnumerable<string> filteredSymbols = symbols;//.Where(s => !s.StartsWith("__"));
+
+                programSymbols.Add(filteredSymbols.ToArray());
+
+                programSymbolTypes.Add(filteredSymbols.Select(s => symbolTable.GetSymbolType(s)).ToArray());
+
+                IUdonSymbolTable entryPointTable = program.EntryPoints;
+
+                IEnumerable<string> filteredEntryPoints = entryPointTable.GetSymbols().Where(s => !s.StartsWith("_onVarChange_"));
+
+                programEntryPoints.Add(filteredEntryPoints.ToArray());
+            }
+
+            Udonity.UdonProgramDataStorage dataStorage = Resources.FindObjectsOfTypeAll<Udonity.UdonProgramDataStorage>().Where(s => s.gameObject.scene.IsValid()).FirstOrDefault();
+
+            if (dataStorage)
+            {
+                Undo.RecordObject(dataStorage, "Generate UdonProgramDataStorage");
+
+                dataStorage.programLookupTable = programInfos.ToArray();
+
+                dataStorage.programSymbols = programSymbols.ToArray();
+
+                dataStorage.programSymbolTypes = programSymbolTypes.ToArray();
+
+                dataStorage.programEntryPoints = programEntryPoints.ToArray();
+            }
+        }
+    }
+}
