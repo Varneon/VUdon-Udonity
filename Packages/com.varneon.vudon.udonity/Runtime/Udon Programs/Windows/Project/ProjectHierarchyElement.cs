631dbefd5053ae968a54d38de8ffbbb11d8457fe
Varneon <me@varneon.com>
2023-05-25 01:01:45 -0700

    Add project window hierarchy element class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/Project/ProjectHierarchyElement.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Windows.Project
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ProjectHierarchyElement : UdonSharpBehaviour
+    {
+        [HideInInspector]
+        public string Directory;
+
+        [HideInInspector]
+        public ProjectWindow ProjectWindow;
+
+        public void Select()
+        {
+            ProjectWindow.OnFolderSelected(Directory);
+        }
+    }
+}
