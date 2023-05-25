341b163310c68b31923748671da8b5180b9356df
Varneon <me@varneon.com>
2023-05-24 23:05:32 -0700

    Add UI foldout script

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/UI Elements/Foldout.cs
@@ -0,0 +1,168 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Varneon.VUdon.Udonity.UIElements
+{
+    [RequireComponent(typeof(LayoutGroup))]
+    [RequireComponent(typeof(ContentSizeFitter))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Foldout : UdonSharpBehaviour
+    {
+        public bool Expanded
+        {
+            get => expanded;
+            set
+            {
+                expanded = value;
+
+                // Shouldn't require a null check, just try to keep UdonSharp happy
+                if (toggle) { toggle.SetExpandedStateWithoutNotify(expanded); }
+
+                SetContentExpandedState(expanded);
+
+                OnExpandedStateChanged();
+            }
+        }
+
+        [Header("Options")]
+        [SerializeField]
+        private bool expanded;
+
+        [Header("References")]
+        [SerializeField]
+        private FoldoutToggle toggle;
+
+        [SerializeField, HideInInspector]
+        private UdonSharpBehaviour expandedStateChangedCallbackReceiver;
+
+        [SerializeField, HideInInspector]
+        private string expandedStateChangedCallbackMethod;
+
+        public void RegisterValueChangedCallback(UdonSharpBehaviour callbackReceiver, string methodName)
+        {
+            expandedStateChangedCallbackReceiver = callbackReceiver;
+
+            expandedStateChangedCallbackMethod = methodName;
+        }
+
+        public void OnExpandedStateChanged()
+        {
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+            return;
+#endif
+
+#pragma warning disable CS0162 // Callback shouldn't be invoked in editor
+            if (expandedStateChangedCallbackReceiver)
+            {
+                expandedStateChangedCallbackReceiver.SendCustomEvent(expandedStateChangedCallbackMethod);
+            }
+#pragma warning restore CS0162
+        }
+
+        private void SetContentExpandedState(bool expanded)
+        {
+            gameObject.SetActive(expanded);
+
+            LayoutGroup[] layoutGroups = GetComponentsInParent<LayoutGroup>(true);
+
+            foreach (LayoutGroup layoutGroup in layoutGroups)
+            {
+                LayoutRebuilder.ForceRebuildLayoutImmediate(layoutGroup.GetComponent<RectTransform>());
+            }
+        }
+
+        public void SetExpandedStateWithoutNotify(bool value)
+        {
+            expanded = value;
+
+            toggle.SetExpandedStateWithoutNotify(expanded);
+
+            SetContentExpandedState(expanded);
+        }
+
+//#if UNITY_EDITOR && !COMPILER_UDONSHARP
+//        private void OnValidate()
+//        {
+//            UnityEditor.EditorApplication.delayCall += OnValidateDelayed;
+//        }
+
+//        private void OnValidateDelayed()
+//        {
+//            if (this == null) { return; }
+
+//            if (toggle)
+//            {
+//                toggle.Foldout = this;
+
+//                toggle.SetExpandedStateWithoutNotify(expanded);
+//            }
+
+//            SetContentExpandedState(expanded);
+//        }
+//#endif
+    }
+}
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+namespace Varneon.VUdon.Udonity.UIElements.Editor
+{
+    using UnityEditor;
+
+    [CustomEditor(typeof(Foldout))]
+    public class FoldoutEditor : Editor
+    {
+        private Foldout foldout;
+
+        private SerializedProperty
+            expandedProperty,
+            toggleProperty;
+
+        private void OnEnable()
+        {
+            foldout = (Foldout)target;
+
+            expandedProperty = serializedObject.FindProperty("expanded");
+
+            toggleProperty = serializedObject.FindProperty("toggle");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope())
+            {
+                EditorGUILayout.PropertyField(toggleProperty);
+
+                if (scope.changed)
+                {
+                    serializedObject.ApplyModifiedProperties();
+
+                    FoldoutToggle foldoutToggle = toggleProperty.objectReferenceValue as FoldoutToggle;
+
+                    if (foldoutToggle)
+                    {
+                        Undo.RecordObject(foldoutToggle, "Assign Foldout");
+
+                        foldoutToggle.Foldout = foldout;
+                    }
+                }
+            }
+
+            using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope())
+            {
+                EditorGUILayout.PropertyField(expandedProperty);
+
+                if (scope.changed)
+                {
+                    serializedObject.ApplyModifiedProperties();
+
+                    if (toggleProperty.objectReferenceValue)
+                    {
+                        foldout.Expanded = expandedProperty.boolValue;
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/UI Elements/FoldoutToggle.cs
@@ -0,0 +1,138 @@
+﻿using UdonSharp;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Varneon.VUdon.Udonity.UIElements
+{
+    [RequireComponent(typeof(Toggle))]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class FoldoutToggle : UdonSharpBehaviour
+    {
+        [HideInInspector]
+        public Foldout Foldout;
+
+        [SerializeField]
+        private bool expanded;
+
+        [SerializeField, HideInInspector]
+        private Toggle toggle;
+
+        [SerializeField, HideInInspector]
+        private RectTransform imageTransform;
+
+        private Vector3 imageExpandedAngle = new Vector3(0f, 0f, -90f);
+
+        public void ToggleExpanded()
+        {
+            expanded = toggle.isOn;
+
+            SetImageExpandedState(expanded);
+
+            if (Foldout)
+            {
+                Foldout.Expanded = expanded;
+            }
+        }
+
+        public void SetExpandedStateWithoutNotify(bool value)
+        {
+            if(expanded == value) { return; }
+
+            expanded = value;
+
+            toggle.SetIsOnWithoutNotify(expanded);
+
+            SetImageExpandedState(expanded);
+        }
+
+        private void SetImageExpandedState(bool expanded)
+        {
+            imageTransform.localEulerAngles = expanded ? imageExpandedAngle : Vector3.zero;
+        }
+
+//#if UNITY_EDITOR && !COMPILER_UDONSHARP
+//        private void OnValidate()
+//        {
+//            EditorApplication.delayCall += OnValidateDelayed;
+//        }
+
+//        private void OnValidateDelayed()
+//        {
+//            if (this == null) { return; }
+
+//            if (toggle == null) { toggle = GetComponent<Toggle>(); }
+
+//            if (toggle)
+//            {
+//                toggle.SetIsOnWithoutNotify(expanded);
+//            }
+
+//            if (imageTransform == null) { imageTransform = toggle.targetGraphic.rectTransform; }
+
+//            if (Foldout)
+//            {
+//                Foldout.Expanded = expanded;
+//            }
+
+//            if (imageTransform)
+//            {
+//                SetImageExpandedState(expanded);
+//            }
+//        }
+//#endif
+    }
+}
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+namespace Varneon.VUdon.Udonity.UIElements.Editor
+{
+    using UnityEditor;
+
+    [CustomEditor(typeof(FoldoutToggle))]
+    public class FoldoutToggleEditor : Editor
+    {
+        private FoldoutToggle foldoutToggle;
+
+        private SerializedProperty expandedProperty;
+
+        private void OnEnable()
+        {
+            foldoutToggle = (FoldoutToggle)target;
+
+            expandedProperty = serializedObject.FindProperty("expanded");
+
+            SerializedProperty toggleProperty = serializedObject.FindProperty("toggle");
+
+            if(toggleProperty.objectReferenceValue == null)
+            {
+                toggleProperty.objectReferenceValue = foldoutToggle.GetComponent<Toggle>();
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (EditorGUI.ChangeCheckScope scope = new EditorGUI.ChangeCheckScope())
+            {
+                EditorGUILayout.PropertyField(expandedProperty);
+
+                if (scope.changed)
+                {
+                    serializedObject.ApplyModifiedProperties();
+
+                    bool expanded = expandedProperty.boolValue;
+
+                    foldoutToggle.SetExpandedStateWithoutNotify(expanded);
+
+                    if (foldoutToggle.Foldout)
+                    {
+                        foldoutToggle.Foldout.SetExpandedStateWithoutNotify(expanded);
+                    }
+                }
+            }
+        }
+    }
+}
+#endif
