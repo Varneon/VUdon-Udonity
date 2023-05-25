99854999d45a2160df292e2721eb336b35f328f7
Varneon <me@varneon.com>
2023-05-25 00:24:45 -0700

    Add abstract window layout element class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/WindowLayoutElement.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public abstract class WindowLayoutElement : UdonSharpBehaviour
+    {
+        public Vector2 SizeDelta
+        {
+            get => rectTransform.sizeDelta;
+            set => rectTransform.sizeDelta = value;
+        }
+
+        public Vector2 AnchoredPosition
+        {
+            get => rectTransform.anchoredPosition;
+            set => rectTransform.anchoredPosition = value;
+        }
+
+        public RectTransform RectTransform => rectTransform;
+
+        public Vector2 MinSize => minSize;
+
+        [SerializeField, HideInInspector]
+        internal protected RectTransform rectTransform;
+
+        protected Vector2 minSize = Vector2.zero;
+    }
+}
