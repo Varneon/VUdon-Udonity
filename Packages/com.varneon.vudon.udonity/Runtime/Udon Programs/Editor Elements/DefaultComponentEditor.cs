16688004bee406eb211e366e75e1923d2d78e2a2
Varneon <me@varneon.com>
2023-05-24 23:17:59 -0700

    Add default component editor class

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Editor Elements/DefaultComponentEditor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Editors
+{
+    public class DefaultComponentEditor : Abstract.ComponentEditor
+    {
+        [SerializeField]
+        private TextMeshProUGUI nameText;
+
+        public override Type InspectedType => typeof(Component);
+
+        protected override void OnEditorInitialized(Component component)
+        {
+            if(component == null) { return; }
+
+            Type type = component.GetType();
+
+            string fullName = type.FullName;
+
+            nameText.text = fullName.Contains(".") ? string.Format("<color=#888>{0}</color>.{1}", fullName.Substring(0, fullName.LastIndexOf('.')), type.Name) : fullName;
+        }
+
+        internal void OverrideTypeName(string typeName)
+        {
+            nameText.text = $"{typeName} <color=#844>{nameText.text}</color>";
+        }
+    }
+}
