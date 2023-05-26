6cc4caf314eca03305332980b470877f1d87d6b0
Varneon <me@varneon.com>
2023-05-26 03:03:00 -0700

    Add basic undo

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Internal/UdonUndo.cs
@@ -0,0 +1,95 @@
+﻿using JetBrains.Annotations;
+using UdonSharp;
+using UnityEngine;
+using Varneon.VUdon.Udonity.Enums;
+
+namespace Varneon.VUdon.Udonity
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class UdonUndo : UdonSharpBehaviour
+    {
+        [SerializeField, Range(8, 128)]
+        private int bufferSize = 32;
+
+        /// <summary>
+        /// <para>0: (Object) Target</para>
+        /// <para>1: (Enum) Target undo type</para>
+        /// <para>2: (String) Name</para>
+        /// <para>3+: (object[]) Recorded values</para>
+        /// </summary>
+        private object[][] undoBuffer;
+
+        /// <summary>
+        /// Rolling start index of the undo buffer
+        /// </summary>
+        private int bufferStart;
+
+        /// <summary>
+        /// Rolling end index of the undo buffer
+        /// </summary>
+        private int bufferEnd;
+
+        /// <summary>
+        /// Current index of the undo buffer
+        /// </summary>
+        private int bufferIndex;
+
+        private void Start()
+        {
+            undoBuffer = new object[bufferSize][];
+        }
+
+        [PublicAPI]
+        public object BeginRecordObject(Transform transformToUndo, TransformUndoType type)
+        {
+            return UdonUndoHandler.GetRecordedValues(transformToUndo, type);
+        }
+
+        [PublicAPI]
+        public void EndRecordObject(Transform transformToUndo, TransformUndoType type, string name, object startValue)
+        {
+            AddRecord(transformToUndo, (int)type, name, startValue, UdonUndoHandler.GetRecordedValues(transformToUndo, type));
+        }
+
+        [PublicAPI]
+        public object BeginRecordObject(GameObject gameObjectToUndo, GameObjectUndoType type)
+        {
+            return UdonUndoHandler.GetRecordedValues(gameObjectToUndo, type);
+        }
+
+        [PublicAPI]
+        public void EndRecordObject(GameObject gameObjectToUndo, GameObjectUndoType type, string name, object startValue)
+        {
+            AddRecord(gameObjectToUndo, (int)type, name, startValue, UdonUndoHandler.GetRecordedValues(gameObjectToUndo, type));
+        }
+
+        private void AddRecord(Object objectToUndo, int type, string name, object startValue, object endValue)
+        {
+            if (bufferIndex < bufferEnd) { bufferEnd = bufferIndex; }
+
+            if (++bufferEnd >= bufferStart + bufferSize) { bufferStart++; }
+
+            undoBuffer[bufferIndex++ % bufferSize] = new object[] { objectToUndo, type, name, startValue, endValue };
+        }
+
+        [PublicAPI]
+        public void PerformUndo()
+        {
+            if(bufferIndex == bufferStart) { Debug.LogWarning("Reached buffer's start!"); return; }
+
+            object[] record = undoBuffer[--bufferIndex % bufferSize];
+
+            UdonUndoHandler.PerformUndo(record);
+        }
+
+        [PublicAPI]
+        public void PerformRedo()
+        {
+            if(bufferIndex == bufferEnd) { Debug.LogWarning("Reached buffer's end!"); return; }
+
+            object[] record = undoBuffer[bufferIndex++ % bufferSize];
+
+            UdonUndoHandler.PerformRedo(record);
+        }
+    }
+}
---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Internal/UdonUndoHandler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+using Varneon.VUdon.Udonity.Enums;
+
+namespace Varneon.VUdon.Udonity
+{
+    public static class UdonUndoHandler
+    {
+        internal static void PerformUndo(object[] record)
+        {
+            object objectToUndo = record[0];
+
+            if (objectToUndo == null) { return; }
+
+            ApplyUndoValues(objectToUndo, (int)record[1], record[3]);
+        }
+
+        internal static void PerformRedo(object[] record)
+        {
+            object objectToRedo = record[0];
+
+            if (objectToRedo == null) { return; }
+
+            ApplyUndoValues(objectToRedo, (int)record[1], record[4]);
+        }
+
+        private static void ApplyUndoValues(object objectToUndo, int undoType, object values)
+        {
+            Type objectType = objectToUndo.GetType();
+
+            if (objectType.Equals(typeof(GameObject)))
+            {
+                ApplyUndoValues((GameObject)objectToUndo, (GameObjectUndoType)undoType, values);
+            }
+            else if (objectType.Equals(typeof(Transform)))
+            {
+                ApplyUndoValues((Transform)objectToUndo, (TransformUndoType)undoType, values);
+            }
+        }
+
+        private static void ApplyUndoValues(GameObject gameObjectToUndo, GameObjectUndoType undoType, object values)
+        {
+            switch (undoType)
+            {
+                case GameObjectUndoType.Active:
+                    gameObjectToUndo.SetActive((bool)values);
+                    break;
+                case GameObjectUndoType.Name:
+                    gameObjectToUndo.name = (string)values;
+                    break;
+                case GameObjectUndoType.Layer:
+                    gameObjectToUndo.layer = (int)values;
+                    break;
+            }
+        }
+
+        private static void ApplyUndoValues(Transform transformToUndo, TransformUndoType undoType, object values)
+        {
+            switch (undoType)
+            {
+                case TransformUndoType.LocalPosition:
+                    transformToUndo.localPosition = (Vector3)values;
+                    break;
+                case TransformUndoType.LocalRotation:
+                    transformToUndo.localRotation = (Quaternion)values;
+                    break;
+                case TransformUndoType.LocalScale:
+                    transformToUndo.localScale = (Vector3)values;
+                    break;
+            }
+        }
+
+        internal static object GetRecordedValues(GameObject gameObjectToUndo, GameObjectUndoType undoType)
+        {
+            switch (undoType)
+            {
+                case GameObjectUndoType.Active:
+                    return gameObjectToUndo.activeSelf;
+                case GameObjectUndoType.Name:
+                    return gameObjectToUndo.name;
+                case GameObjectUndoType.Layer:
+                    return gameObjectToUndo.layer;
+                default:
+                    return null;
+            }
+        }
+
+        internal static object GetRecordedValues(Transform transformToUndo, TransformUndoType undoType)
+        {
+            switch (undoType)
+            {
+                case TransformUndoType.LocalPosition:
+                    return transformToUndo.localPosition;
+                case TransformUndoType.LocalRotation:
+                    return transformToUndo.localRotation;
+                case TransformUndoType.LocalScale:
+                    return transformToUndo.localScale;
+                default:
+                    return null;
+            }
+        }
+    }
+}
