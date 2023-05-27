ad0ddbb494e1a6f4191f19d0ac4d723179a93dbf
Varneon <me@varneon.com>
2023-05-26 20:10:39 -0700

    Add editor utilities class

---
+++ Packages/com.varneon.vudon.udonity/Editor/UdonityEditorUtilities.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Varneon.VUdon.Udonity.Editor
+{
+    public static class UdonityEditorUtilities
+    {
+        public static string UDONITY_EDITOR_DESCRIPTOR_PREFAB_PATH = "Packages/com.varneon.vudon.udonity/Runtime/Prefabs/UdonityEditor.prefab";
+
+        [MenuItem("Varneon/VUdon/Udonity/Add Udonity To Scene")]
+        private static void AddUdonityEditorDescriptorToScene()
+        {
+            AddUdonityEditorDescriptorToScene(true);
+        }
+
+        public static void AddUdonityEditorDescriptorToScene(bool setAsActiveSelection)
+        {
+            Scene activeScene = SceneManager.GetActiveScene();
+
+            GameObject descriptorPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(UDONITY_EDITOR_DESCRIPTOR_PREFAB_PATH);
+
+            GameObject descriptorInstance = PrefabUtility.InstantiatePrefab(descriptorPrefab, activeScene) as GameObject;
+
+            if (setAsActiveSelection)
+            {
+                Selection.activeGameObject = descriptorInstance;
+            }
+        }
+    }
+}
