6c9e2537322326384bd6ee01e56044edfd08f4c2
Varneon <me@varneon.com>
2024-03-03 21:16:47 -0800

    Add UdonMonitor symbol list item behaviour

---
+++ Packages/com.varneon.vudon.udonity/Runtime/Udon Programs/Windows/Udon Monitor/UdonMonitorSymbolElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+namespace Varneon.VUdon.Udonity.Windows.UdonMonitor
+{
+    [AddComponentMenu("")]
+    [DisallowMultipleComponent]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class UdonMonitorSymbolElement : UdonSharpBehaviour
+    {
+        private Type symbolType;
+
+        private string symbolName;
+
+        internal void Initialize(string symbolName, Type symbolType)
+        {
+            this.symbolName = symbolName;
+            this.symbolType = symbolType;
+        }
+
+        public void SelectSymbol()
+        {
+            GetComponentInParent<UdonMonitorWindow>().SelectSymbol(symbolName, symbolType);
+        }
+    }
+}
